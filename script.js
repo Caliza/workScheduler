c826b3b0a96d22ace56863534aa8e9319882b36b
Caliza <rances.rodriguez@gmail.com>
2023-01-14 23:00:31 -0800

    Redacted code to set and get user input to local storage.

--- script.js
+++ script.js
@@ -7,37 +7,36 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should --Hint: assign eventListener to container block
   //$. btnEL = document.getElementById("#btn");
-// var inText =document.getElementsByClassName("description");
-// var saveBtn = document.getElementsByClassName("saveBtn");
+  // var inText =document.getElementsByClassName("description");
+  // var saveBtn = document.getElementsByClassName("saveBtn");
 
-var eventInput = $('#event');
-// var btnClick = $('#btnB');
+  var eventInput = $('.description');
+  // var btnClick = $('#btnB');
 
-function saveToStorage() {
-  
-  var passText =eventInput[0].value;
-  localStorage.setItem("passText", JSON.stringify(passText));
-};
+  function saveToStorage() {
 
-function getFromStorage() {
-  var eventOutput = JSON.parse(localStorage.getItem("passText"));
-  if (eventOutput !== null) {
-  document.getElementById('event').innerHTML = eventOutput.textInput;
-  } else {
-    return;
-  }
-};
+    var eventText = eventInput[0].value;
+    localStorage.setItem("eventText", JSON.stringify(eventText));
+  };
 
+  function getFromStorage() {
+    var eventOutput = localStorage.getItem("eventText");
+    if (eventOutput !== null) {
+      document.getElementById('event').innerHTML = eventOutput;
+    } else {
+      return;
+    }
+  };
+  getFromStorage();
 
-    $('#btnX').on("click", function(){
-      saveToStorage();
-      getFromStorage();
-        console.log("rad");
-    });
- 
+  $('#btnX').on("click", function (event) {
+    saveToStorage(event);
+    console.log("rad");
+  });
+
 
   // use the id in the containing time-block as a key to save the user input in --Hint: grab value from time block/id
-  
+
   // local storage. HINT: What does `this` reference in the click listener --Option 1: store each timeBlock as seperate key(a,b,c,) value pair. --Option 2 store array,
   // function? How can DOM traversal be used to get the "hour-x" id of the --Sibling or child
   // time-block containing the button that was clicked? How might the id be
@@ -55,12 +54,12 @@ function getFromStorage() {
   //
   // TODO: Add code to display the current date in the header of the page.
   // var currentDay = document.getElementById("#currentDay")
-// function formatDate(dateObject){
+  // function formatDate(dateObject){
 
 
-const dayJsObject = dayjs();
-// console.log(dayJsObject.format("D"))
-$("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
+  const dayJsObject = dayjs();
+  // console.log(dayJsObject.format("D"))
+  $("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
 });
 
 // $('#btnX').on("click", function(event){
