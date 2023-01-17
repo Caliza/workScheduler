0369f40fe8557a3089e9f8411d3f9f37f1b5f4b5
Caliza <rances.rodriguez@gmail.com>
2023-01-17 12:08:25 -0800

    For loop and var to check current time and set color-class to time blocks.

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ $(function () {
       localStorage.setItem(time, value);
       console.log("rad");
     })
+  })
+  
 
     // Lines 29-37 gets text input from each LocalStorage and passes it corresponding <textarea>.
 
@@ -36,58 +38,18 @@ $(function () {
     $('#hour-16 .description').val(localStorage.getItem('hour-16'));
     $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-
-
-    // TODO: Add a listener for click events on the save button. This code should --Hint: assign eventListener to container block
-  
-    // use the id in the containing time-block as a key to save the user input in --Hint: grab value from time block/id
-
-    // local storage. HINT: What does `this` reference in the click listener --Option 1: store each timeBlock as seperate key(a,b,c,) value pair. --Option 2 store array,
-    // function? How can DOM traversal be used to get the "hour-x" id of the --Sibling or child
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set --should saved when button clicked
-    // the values of the corresponding textarea elements. HINT: How can the id --be targeted <textarea>
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-    // var currentDay = document.getElementById("#currentDay")
-    // function formatDate(dateObject){
-
-
     const dayJsObject = dayjs();
     $("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
-  });
-});
 
-// Save!!! 
-    // var eventInput = $("#event");
-
-    // function saveToStorage() {
-
-    //   var eventText = eventInput[0].value;
-    //   localStorage.setItem("eventText", JSON.stringify(eventText));
-    // };
-
-    // function getFromStorage() {
-    //   var eventOutput = localStorage.getItem("eventText");
-    //   if (eventOutput !== null) {
-    //     ---document.getElementById('event').innerHTML = eventOutput;
+    // let currentHour = dayjs().hours()
+    // console.log(currentHour)
+    // for (let i = 9; i <= 17; i++) {
+    //   if (i < currentHour) {
+    //     $("#hour-" + i).children("textarea").addClass("past")
+    //   } else if (i == currentHour) {
+    //     $("#hours-" + i).children("textarea").addClass("present")
     //   } else {
-    //     return;
+    //     $("#hours-" + i).children("textarea").addClass("present")
     //   }
-    // };
-    // getFromStorage();
 
-    // $('#btnX').on("click", function (event) {
-    //   saveToStorage(event);
-    //   console.log("rad");
-    // }); 
-    //Save!!!
+});
