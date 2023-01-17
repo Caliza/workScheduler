dd8dda7362db3a43ddc1cd02064926939a641213
Caliza <rances.rodriguez@gmail.com>
2023-01-16 23:27:00 -0800

    Code added to set and get item form local storage.

--- script.js
+++ script.js
@@ -9,80 +9,96 @@ $(function () {
   //$. btnEL = document.getElementById("#btn");
   // var inText =document.getElementsByClassName("description");
   // var saveBtn = document.getElementsByClassName("saveBtn");
-// var eventInput = document.querySelectorAll(".description");
-//   eventInput.forEach(()=>{
-//     dataFromLocalStorage = localStorage.getItem("key")
-//     $(this).textContent = dataFromLocalStorage
-// })
+  // var eventInput = document.querySelectorAll(".description");
+  //   eventInput.forEach(()=>{
+  //     dataFromLocalStorage = localStorage.getItem("key")
+  //     $(this).textContent = dataFromLocalStorage
+  // })
 
-//
-var value = $(this)
-      .siblings('.description')
-      .val();
-    var time = $(this)
-      .parent()
-      .attr('#event');
-
-localStorage.getItem(time, value);
-
-$(document).ready(function() {
-$('.saveBtn').on('click', function() {
-  localStorage.setItem(time, value);
-  console.log("rad");
-})
-
-  // Save!!! 
-  // var eventInput = $("#event");
-
-  // function saveToStorage() {
-
-  //   var eventText = eventInput[0].value;
-  //   localStorage.setItem("eventText", JSON.stringify(eventText));
-  // };
-
-  // function getFromStorage() {
-  //   var eventOutput = localStorage.getItem("eventText");
-  //   if (eventOutput !== null) {
-  //     document.getElementById('event').innerHTML = eventOutput;
-  //   } else {
-  //     return;
-  //   }
-  // };
-  // getFromStorage();
-
-  // $('#btnX').on("click", function (event) {
-  //   saveToStorage(event);
-  //   console.log("rad");
-  // }); 
-  //Save!!!
-
-  
-  // use the id in the containing time-block as a key to save the user input in --Hint: grab value from time block/id
-
-  // local storage. HINT: What does `this` reference in the click listener --Option 1: store each timeBlock as seperate key(a,b,c,) value pair. --Option 2 store array,
-  // function? How can DOM traversal be used to get the "hour-x" id of the --Sibling or child
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set --should saved when button clicked
-  // the values of the corresponding textarea elements. HINT: How can the id --be targeted <textarea>
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  // var currentDay = document.getElementById("#currentDay")
-  // function formatDate(dateObject){
+  var value = $(this)
+    .siblings('.description')
+    .val();
+  var time = $(this)
+    .parent()
+    .attr('id');
+
+$(function () { 
+    var value = localStorage.getItem(time, value)
+    $('.description').text(value) 
+  //   .siblings('.description')
+  //   .val();
+  // var time = $(this)
+  //   .parent()
+  //   .attr('id');   
+  })
+
+  $(document).ready(function () {
+    $('.saveBtn').on('click', function () {
+      var value = $(this)
+        .siblings('.description')
+        .val();
+      var time = $(this)
+        .parent()
+        .attr('id');
+      console.log("time", time)
+      console.log("value", value)
+      localStorage.setItem(time, value);
+      console.log("rad");
+    })
+
+    // Save!!! 
+    // var eventInput = $("#event");
+
+    // function saveToStorage() {
+
+    //   var eventText = eventInput[0].value;
+    //   localStorage.setItem("eventText", JSON.stringify(eventText));
+    // };
+
+    // function getFromStorage() {
+    //   var eventOutput = localStorage.getItem("eventText");
+    //   if (eventOutput !== null) {
+    //     ---document.getElementById('event').innerHTML = eventOutput;
+    //   } else {
+    //     return;
+    //   }
+    // };
+    // getFromStorage();
+
+    // $('#btnX').on("click", function (event) {
+    //   saveToStorage(event);
+    //   console.log("rad");
+    // }); 
+    //Save!!!
 
 
-  const dayJsObject = dayjs();
-  // console.log(dayJsObject.format("D"))
-  $("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
-});
+    // use the id in the containing time-block as a key to save the user input in --Hint: grab value from time block/id
+
+    // local storage. HINT: What does `this` reference in the click listener --Option 1: store each timeBlock as seperate key(a,b,c,) value pair. --Option 2 store array,
+    // function? How can DOM traversal be used to get the "hour-x" id of the --Sibling or child
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+    //
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    //
+    // TODO: Add code to get any user input that was saved in localStorage and set --should saved when button clicked
+    // the values of the corresponding textarea elements. HINT: How can the id --be targeted <textarea>
+    // attribute of each time-block be used to do this?
+    //
+    // TODO: Add code to display the current date in the header of the page.
+    // var currentDay = document.getElementById("#currentDay")
+    // function formatDate(dateObject){
+
+
+    const dayJsObject = dayjs();
+    // console.log(dayJsObject.format("D"))
+    $("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
+  });
 });
 // $('#btnX').on("click", function(event){
 //   console.log(event);
