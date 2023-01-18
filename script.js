48a808b2e19e88434b1d9f994a5f1399510d1fde
Caliza <rances.rodriguez@gmail.com>
2023-01-18 11:50:26 -0800

    Added for loop to that checks currenHour and sets class/background colors of each time slot based on the current time.

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// Wrap (all code/vars inside function) all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-// text9, text 10, etc
 
-$(function () {
+  // Target the DOM with the .ready()method to run JavaScript as soon as the pages DOM is ready to be manipulated.
 
-  // Target the DOM with the .ready()method to run JavaScript as soon as the pages DOM is ready to manipulate.
-
-  // Taking text input from <textarea> with $(this) method and defining it to var value & time; sets it into LocalStorage.
+  //  $(this) takes text input from <textarea> and defines it as var 'value' & 'time'; sets it into LocalStorage.
 
   $(document).ready(function () {
     $('.saveBtn').on('click', function () {
@@ -18,38 +12,39 @@ $(function () {
       var time = $(this)
         .parent()
         .attr('id');
-      console.log("time", time)
-      console.log("value", value)
+      console.log('time', time)
+      console.log('value', value)
       localStorage.setItem(time, value);
-      console.log("rad");
+      console.log('rad');
     })
   })
-  
 
-    // Lines 29-37 gets text input from each LocalStorage and passes it corresponding <textarea>.
 
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  // Lines 29-37 gets text input set to LocalStorage and passes it to its corresponding <textarea>.
 
-    const dayJsObject = dayjs();
-    $("#currentDay").html(dayJsObject.format("dddd YYYY-MM-DD HH:mm:ss"))
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-    // let currentHour = dayjs().hours()
-    // console.log(currentHour)
-    // for (let i = 9; i <= 17; i++) {
-    //   if (i < currentHour) {
-    //     $("#hour-" + i).children("textarea").addClass("past")
-    //   } else if (i == currentHour) {
-    //     $("#hours-" + i).children("textarea").addClass("present")
-    //   } else {
-    //     $("#hours-" + i).children("textarea").addClass("present")
-    //   }
+  // dayJsObject is used to sett and display the date and time in the browser. currentHour is passed thruogh the for loop to check current time and set bakcground class/colors of the time slots based on .past, .present, .future.
 
-});
+  const dayJsObject = dayjs();
+  $('#currentDay').html(dayJsObject.format('dddd YYYY-MM-DD HH:mm:ss'))
+
+  let currentHour = dayjs().hour()
+  console.log(currentHour)
+  for (let i = 9; i <= 17; i++) {
+    if (i < currentHour) {
+      $('#hour-' + i).children('textarea').addClass('past')
+    } else if (i == currentHour) {
+      $('#hour-' + i).children('textarea').addClass('present')
+    } else {
+      $('#hour-' + i).children('textarea').addClass('future')
+    }
+  }
